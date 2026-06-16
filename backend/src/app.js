a570ce40cd24f39c03904864bc13560fcce6a53e
New Greivin <grarguedasgu@est.utn.ac.cr>
2026-06-16 09:40:15 -0600

    Actualizacion de app.js
    Implementación del uso del  archivo .env, y respuesta como JSON

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,18 @@
+import "dotenv/config"; //Si la ruta del .env en otro .env, "dotenv/config(.env.development)" 
 import express from "express";
 
+const { NAME, VERSION, DESCRIPTION, AUTHOR } = process.env;
+
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
-  res.send(`<p>Versión: Funcionando</p>`);
+ res.json({
+    name: NAME,
+    version: VERSION,
+    description: DESCRIPTION,
+    author: AUTHOR,
+ })
 });
 
 app.listen(PORT, () => {
