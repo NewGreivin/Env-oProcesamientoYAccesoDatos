a2c4616e68e11cd100fbd566635ef5d232e59d69
New Greivin <grarguedasgu@est.utn.ac.cr>
2026-06-16 10:09:26 -0600

    Implementación de cors para exponer la API
    - Contiene la instalación de cors
    - Con ello se logra exponer la api gracias al cors, y usando el express.json

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,17 @@
 import "dotenv/config"; //Si la ruta del .env en otro .env, "dotenv/config(.env.development)" 
 import express from "express";
+import cors from "cors"; //Instalacion de cors para que front-end pueda acceder a la API
 
 const { NAME, VERSION, DESCRIPTION, AUTHOR } = process.env;
 
 const app = express();
+app.use(cors()); //Permite que otros origenes puedan acceder a la API (Integracion de cors necesaria)
+app.use(express.json()); //Permite que el front-end pueda enviar datos en formato JSON (Integracion de cors necesaria)
+
 const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
- res.json({
+ res.json({ //Devuelve un objeto JSON, para que el front-end pueda leer la respuesta y mostrarla
     name: NAME,
     version: VERSION,
     description: DESCRIPTION,
@@ -16,6 +20,6 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`${process.env.NAME} ejecutándose en http://localhost:${PORT}`
+  console.log(`${NAME} ejecutándose en http://localhost:${PORT}`
   );
 });
