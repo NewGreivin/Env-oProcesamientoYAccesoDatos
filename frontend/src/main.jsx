f387895e441df3ea24abbe68150d87af4cfc9421
New Greivin <grarguedasgu@est.utn.ac.cr>
2026-06-23 07:48:47 -0600

    Creación de archivos principales
    Contiene los archivos de App, main esenciales del proyecto

---
+++ frontend/src/main.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css"; //npm instal bootstrap
+import "bootstrap-icons/font/bootstrap-icons.css"; //npm instal bootstrap-icons
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+);
